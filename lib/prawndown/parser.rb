11be892909941d00fd75bec1358bb70ff2c7912b
Kasper Meyer <hi@kaspermeyer.com>
2018-03-16 13:30:29 +0100

    Fix typos in documentation

--- lib/prawndown/parser.rb
+++ lib/prawndown/parser.rb
@@ -4,9 +4,9 @@ module Prawndown
       /^# (.+)/                  => '<font size="26"><b>\1</b></font>', # Header 1
       /^## (.+)/                 => '<font size="24"><b>\1</b></font>', # Header 2
       /^### (.+)/                => '<font size="22"><b>\1</b></font>', # Header 3
-      /^#### (.+)/               => '<font size="20"><b>\1</b></font>', # Header 3
-      /^##### (.+)/              => '<font size="18"><b>\1</b></font>', # Header 3
-      /^###### (.+)/             => '<font size="16"><b>\1</b></font>', # Header 3
+      /^#### (.+)/               => '<font size="20"><b>\1</b></font>', # Header 4
+      /^##### (.+)/              => '<font size="18"><b>\1</b></font>', # Header 5
+      /^###### (.+)/             => '<font size="16"><b>\1</b></font>', # Header 6
       /\[([^\[]+)\]\(([^\)]+)\)/ => '<link href="\2">\1</link>',        # Link
       /(\*\*|__)(.*?)\1/         => '<b>\2</b>',                        # Bold
       /(\*|_)(.*?)\1/            => '<i>\2</i>',                        # Italic
